912515ce2aa896ea8a2993a8dd4939b31bd28878
Alberts Liepins <liepins01@gmail.com>
2022-09-17 21:39:23 +0300

    .

--- dorsly/src/firebase.js
+++ dorsly/src/firebase.js
@@ -1,11 +1,9 @@
 // Import the functions you need from the SDKs you need
 import { getAnalytics } from "firebase/analytics"
 import { initializeApp } from "firebase/app"
-// TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: import.meta.env.API_KEY,
   authDomain: import.meta.env.AUTH_DOMAIN,
