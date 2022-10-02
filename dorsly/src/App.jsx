99e9a7358532968804be7c2f4b8df3b4c3847661
Alberts Liepins <liepins01@gmail.com>
2022-10-02 23:30:53 +0300

    docs: Added disclosre statement for the warning.

--- dorsly/src/App.jsx
+++ dorsly/src/App.jsx
@@ -25,7 +25,10 @@ function App() {
         </p>
       </div>
       <p className="read-the-docs">
-        This is a student group project, still a work in progress. <strong>All current and future listed products, services, etc, ARE NOT REAL PRODUCTS OR SERVICES</strong>, all listed products, services, etc, are example products indended to showcase the payment system for the purposes of a school project, without exceptions. We seriously suggest that you <strong>do not make payments through our system unless you are informed of the purpose of the project.</strong>
+        This is a student group project, still a work in progress. <strong>All current and future listed products, services, etc, ARE NOT REAL PRODUCTS OR SERVICES</strong>, all listed products, services, etc, are example products intended to showcase the payment system for the purposes of a school project, without exceptions. We seriously suggest that you <strong>do not make payments through our system unless you are informed of the purpose of the project.</strong>
+        <br/>
+        <br/>
+        This warning has been added to comply with <span style={{'fontWeight': 550, 'color': '#6772E5'}}>Stripe</span> payment gateway terms of use.
       </p>
     </div>
   )
