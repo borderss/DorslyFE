b2acc7fa5822bde7fb5d3373e09f948ce1ea61cc
roberttss <robertsabolins772@gmail.com>
2022-11-28 17:58:31 +0200

    wip  added link to navbar

--- dorsly/src/components/header.jsx
+++ dorsly/src/components/header.jsx
@@ -10,20 +10,32 @@ export default function header() {
     <div className="navbar">
       <div className="wraper">
         <div className="left-side">
+          <a href="#">
           <div className="dorsly-logo">
             <img src={LogoIcon} alt="Dorsly" />
           </div>
+          </a>
+          <a href="about_us">
           <div className="about-us">About us</div>
+          </a>
+          <a href="contact">
           <div className="contact">Contact</div>
+          </a>
+          <a href="reserve_now">
           <div className="reserve-now">Reserve now</div>
+          </a>
           <div className="search-logo">
             <img src={SearchIcon} alt="Search" />
           </div>
         </div>
 
         <div className="right-side">
+          <a href="log_in">
           <div className="log-in">Log in</div>
+          </a>
+          <a href="register">
           <div className="register">Register</div>
+          </a>
           <div className="gps-logo">
             <img src={GpsIcon} alt="GPS" />
           </div>
