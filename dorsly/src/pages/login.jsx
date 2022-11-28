f6d2fec88f454829410c77a4cc3fb2296cf78871
Alberts Liepins <liepins01@gmail.com>
2022-11-28 11:15:21 +0200

    wip: login page header, seperator and second section title

--- dorsly/src/pages/login.jsx
+++ dorsly/src/pages/login.jsx
@@ -5,6 +5,8 @@ import "../static/general.css"
 
 import Logo from "/assets/svg/dorslylogo.svg"
 import Illustration from "/assets/svg/illustration.svg"
+import Seperator from "/assets/svg/seperator.svg"
+
 
 export default function login() {
   return (
@@ -15,7 +17,11 @@ export default function login() {
             <img src={Illustration} />
             <img className="logo" src={Logo} />
           </div>
-          <div className="form"></div>
+          <form>
+            <h1>Welcome, we are glad<br/>to have you here</h1>
+            <img className="sperator" src={Seperator} />
+            <h2>Log in</h2>
+          </form>
         </div>
       </div>
     </>
