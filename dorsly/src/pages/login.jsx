7eb064e92f2e91fa5049295ba4bd7ec1401e24eb
Alberts Liepins <liepins01@gmail.com>
2022-12-05 00:50:18 +0200

    feat: input viability indicator and form validation

--- dorsly/src/pages/login.jsx
+++ dorsly/src/pages/login.jsx
@@ -1,4 +1,4 @@
-import React from "react"
+import React, { useEffect, useRef, useState } from "react"
 
 import auth from "../static/css/auth.module.css"
 import "../static/css/general.css"
@@ -7,20 +7,66 @@ import Logo from "/assets/svg/dorslylogo.svg"
 import Illustration from "/assets/svg/illustration.svg"
 import Seperator from "/assets/svg/seperator.svg"
 
+import AppleLogo from "/assets/svg/Apple.svg"
 import FacebookLogo from "/assets/svg/Facebook.svg"
 import GoogleLogo from "/assets/svg/Google.svg"
 import TwitterLogo from "/assets/svg/Twitter.svg"
-import AppleLogo from "/assets/svg/Apple.svg"
-
 
 import LabeledInputField from "../components/labeledInputField"
 
 export default function login() {
+  const [email, setEmail] = useState("")
+  const [password, setPassword] = useState("")
+
+  let submitButtonRef = useRef(null)
+
   const handleLogoButtonClick = () => {
-    window.location.href ='/'
+    window.location.href = "/"
   }
 
-  document.body.style.backgroundImage = 'url("/assets/svg/backgroundlines.svg")';
+  const setLoginButton = () => {
+    if (email && password) {
+      submitButtonRef.current.disabled = false
+    } else {
+      submitButtonRef.current.disabled = true
+    }
+  }
+
+  useEffect(() => {
+    setLoginButton()
+  }, [email, password])
+
+  const handleLogin = () => {
+    window.location.href = "/"
+  }
+
+  const handleInputChange = (inputType, value) => {
+    switch (inputType) {
+      case "email":
+        let emailRegex = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/
+
+        if (value.match(emailRegex)) {
+          setEmail(value)
+          return true
+        } else {
+          setEmail("")
+          return false
+        }
+      case "password":
+        if (value.length > 8) {
+          setPassword(value)
+          return true
+        } else {
+          setPassword("")
+          return false
+        }
+      default:
+        console.warn("invalid input.")
+        break
+    }
+  }
+
+  document.body.style.backgroundImage = 'url("/assets/svg/backgroundlines.svg")'
 
   return (
     <>
@@ -28,24 +74,56 @@ export default function login() {
         <div className={auth["content-field"]}>
           <div className={auth["illustration"]}>
             <img src={Illustration} />
-            <button onClick={e => {handleLogoButtonClick()}}><img className={auth["logo"]} src={Logo} /></button>
+            <button
+              onClick={(e) => {
+                handleLogoButtonClick()
+              }}>
+              <img className={auth["logo"]} src={Logo} />
+            </button>
           </div>
           <form>
-            <h1>Welcome, we are glad<br/>to have you here</h1>
+            <h1>
+              Welcome, we are glad
+              <br />
+              to have you here
+            </h1>
             <img className={auth["sperator"]} src={Seperator} />
             <h2>Log in</h2>
-            <LabeledInputField label="Name" inputName="name"/>
-            <LabeledInputField label="Surname" inputName="surname"/>
 
-            <button className={auth["next-step"]}>Next step</button>
+            <LabeledInputField
+              label="Email"
+              inputName="email"
+              inputType="email"
+              handleInputChange={handleInputChange}
+            />
+            <LabeledInputField
+              label="Password"
+              inputName="password"
+              inputType="password"
+              handleInputChange={handleInputChange}
+            />
+
+            <button ref={submitButtonRef} className={auth["submit"]} disabled>
+              Log in
+            </button>
             <p>or</p>
             <div className={auth["third-party-auth-options"]}>
-              <a href="https://tempurl.com/"><img src={FacebookLogo}/></a>
-              <a href="https://tempurl.com/"><img src={GoogleLogo}/></a>
-              <a href="https://tempurl.com/"><img src={TwitterLogo}/></a>
-              <a href="https://tempurl.com/"><img src={AppleLogo}/></a>
+              <a href="/facebookauth">
+                <img src={FacebookLogo} />
+              </a>
+              <a href="googleauth">
+                <img src={GoogleLogo} />
+              </a>
+              <a href="twitterauth">
+                <img src={TwitterLogo} />
+              </a>
+              <a href="appleauth">
+                <img src={AppleLogo} />
+              </a>
             </div>
-            <p>Already have an account? <a href="/signin">Sign in</a></p>
+            <p>
+              Don't have an account? <a href="/register">Register</a>
+            </p>
           </form>
         </div>
       </div>
