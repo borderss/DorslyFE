a1f1120ca26e5a0ec5534304385f28e01d9e1584
Alberts Liepins <liepins01@gmail.com>
2022-12-11 05:01:53 +0200

    wip: hero section title, desc, groundowrk for search bar

--- dorsly/src/pages/home.jsx
+++ dorsly/src/pages/home.jsx
@@ -3,7 +3,6 @@ import React from "react"
 import "../static/css/general.css"
 import style from "../static/css/home.module.css"
 
-import Footer from "../components/footer"
 import Header from "../components/header"
 import Partner from "../components/partner"
 
@@ -14,13 +13,36 @@ export default function home() {
     <>
       <Header />
 
-      <div className={style["content"]}>
-      <img className={style["phoneIllustration"]} src={PhoneIllustration}/>
+      <div className={style["header-content"]}>
+        <img className={style["phoneIllustration"]} src={PhoneIllustration} />
 
+        <div>
+          <h1>
+            Dont worry about<br/>
+            reservations ever again
+          </h1>
+          <p>
+            We are here to help you make informed decisions about<br />
+            all of your reservations.
+          </p>
+          <div className={style["search-field"]}>
+            <div className={style["date"]}>
+
+            </div>
+            <div className={style["time"]}>
+
+            </div>
+            <div className={style["person-count"]}>
+
+            </div>
+            <div className={style["search"]}>
+
+            </div>
+          </div>
+        </div>
       </div>
 
-
-      <Partner/>
+      <Partner />
     </>
   )
 }
