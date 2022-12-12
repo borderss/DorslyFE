023ec94ff48614b3f369ebf9d497c9933cd22628
Alberts Liepins <liepins01@gmail.com>
2022-12-13 01:20:22 +0200

    feat: navabar styling

--- dorsly/src/components/header.jsx
+++ dorsly/src/components/header.jsx
@@ -1,28 +1,27 @@
 import React from "react"
 import "../static/header.css"
-import LogoIcon from '/assets/svg/dorslyheader.svg'
-import SearchIcon from '/assets/svg/search.svg'
-import GpsIcon from '/assets/svg/gps.svg'
-
+import LogoIcon from "/assets/svg/dorslyheader.svg"
+import GpsIcon from "/assets/svg/gps.svg"
+import SearchIcon from "/assets/svg/search.svg"
 
 export default function header() {
   return (
     <div className="navbar">
       <div className="wraper">
         <div className="left-side">
-          <a href="#">
-          <div className="dorsly-logo">
-            <img src={LogoIcon} alt="Dorsly" />
-          </div>
+          <a href="#" className="logo">
+            <div className="dorsly-logo">
+              <img src={LogoIcon} alt="Dorsly" />
+            </div>
           </a>
           <a href="about_us">
-          <div className="about-us">About us</div>
+            <div className="about-us">About us</div>
           </a>
           <a href="contact">
-          <div className="contact">Contact</div>
+            <div className="contact">Contact</div>
           </a>
           <a href="reserve_now">
-          <div className="reserve-now">Reserve now</div>
+            <div className="reserve-now">Reserve now</div>
           </a>
           <div className="search-logo">
             <img src={SearchIcon} alt="Search" />
@@ -31,10 +30,10 @@ export default function header() {
 
         <div className="right-side">
           <a href="log_in">
-          <div className="log-in">Log in</div>
+            <div className="log-in">Log in</div>
           </a>
           <a href="register">
-          <div className="register">Register</div>
+            <div className="register">Register</div>
           </a>
           <div className="gps-logo">
             <img src={GpsIcon} alt="GPS" />
