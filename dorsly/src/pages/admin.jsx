02da132ab7bfb5c0156462bc3509d9303a1d8981
Alberts Liepins <liepins01@gmail.com>
2022-12-29 04:06:51 +0200

    wip: admin page table design

--- dorsly/src/pages/admin.jsx
+++ dorsly/src/pages/admin.jsx
@@ -1,4 +1,4 @@
-import React, { useContext, useState, useEffect } from "react"
+import React, { useContext, useEffect, useState } from "react"
 
 import Header from "../components/header"
 
@@ -19,49 +19,49 @@ export default function test() {
   const [section, setSection] = useState("accounts")
 
   const [sectionInfo, setSectionInfo] = useState({
-    title: 'Account overview',
-    desc: 'Overview account information such as username, name, surname, email, registration date, date of last change, etc.'
+    title: "Account overview",
+    desc: "Overview account information such as username, name, surname, email, registration date, date of last change, etc.",
   })
 
-  useEffect(()=>{
+  useEffect(() => {
     switch (section) {
       case "accounts":
         setSectionInfo({
-          title: 'Account overview',
-          desc: 'Overview, edit or delete account information such as username, name, surname, email, registration date, date of last change, etc.'
+          title: "Account overview",
+          desc: "Overview, edit or delete account information such as username, name, surname, email, registration date, date of last change, etc.",
         })
-        break;
+        break
 
       case "pointsofinterest":
         setSectionInfo({
-          title: 'Point of interest overview',
-          desc: 'Check interest data, for example, name, description, creation date, number of reservations/pre-purchases fulfilled, and other general satistical data.'
+          title: "Point of interest overview",
+          desc: "Check interest data, for example, name, description, creation date, number of reservations/pre-purchases fulfilled, and other general satistical data.",
         })
-        break;
-    
+        break
+
       case "reservations":
         setSectionInfo({
-          title: 'Reservations overview',
-          desc: 'Search through reservation data, such as the billed person\'s account ID, relevant POI\'s ID, reservation date, time, attending person count, stripe check number, etc.'
+          title: "Reservations overview",
+          desc: "Search through reservation data, such as the billed person's account ID, relevant POI's ID, reservation date, time, attending person count, stripe check number, etc.",
         })
-        break;
-    
+        break
+
       case "prepurchases":
         setSectionInfo({
-          title: 'Pre-purchase overview',
-          desc: 'View pre-purchase data, such as billed person\'s ID, relevant POI\'s ID, associated reservation, the date and time on which the pre-purchase was made, stripe check number, etc.'
+          title: "Pre-purchase overview",
+          desc: "View pre-purchase data, such as billed person's ID, relevant POI's ID, associated reservation, the date and time on which the pre-purchase was made, stripe check number, etc.",
         })
-        break;
-      
+        break
+
       case "reviews":
         setSectionInfo({
-          title: 'POI review overview',
-          desc: 'See point of interest reviews, who posted the review, the date and time the review was posted at, etc.'
+          title: "POI review overview",
+          desc: "See point of interest reviews, who posted the review, the date and time the review was posted at, etc.",
         })
-        break;
-    
+        break
+
       default:
-        break;
+        break
     }
   }, [section])
 
@@ -75,10 +75,11 @@ export default function test() {
     }
 
     setSection(e.target.id)
-
-    ;[...document.querySelectorAll(`.${style["side-navbar"]} > p`)].forEach((el, i) => {
-      el.classList.remove(style["navbar-item-active"])
-    })
+    ;[...document.querySelectorAll(`.${style["side-navbar"]} > p`)].forEach(
+      (el, i) => {
+        el.classList.remove(style["navbar-item-active"])
+      }
+    )
 
     target.classList.toggle(style["navbar-item-active"])
   }
@@ -93,19 +94,35 @@ export default function test() {
             <p>Report any issues to admin@dorsly.com</p>
           </div>
 
-          <p id="accounts" className={style["navbar-item-active"]} style={{ "--nav-item-icon": `url(${PeopleIcon})` }} onClick={e => onNavbarItemClick(e)}>
+          <p
+            id="accounts"
+            className={style["navbar-item-active"]}
+            style={{ "--nav-item-icon": `url(${PeopleIcon})` }}
+            onClick={(e) => onNavbarItemClick(e)}>
             Accounts
           </p>
-          <p id="pointsofinterest" style={{ "--nav-item-icon": `url(${GPSIcon})` }} onClick={e => onNavbarItemClick(e)}>
+          <p
+            id="pointsofinterest"
+            style={{ "--nav-item-icon": `url(${GPSIcon})` }}
+            onClick={(e) => onNavbarItemClick(e)}>
             Points of interest
           </p>
-          <p id="reservations" style={{ "--nav-item-icon": `url(${CalendarIcon})` }} onClick={e => onNavbarItemClick(e)}>
+          <p
+            id="reservations"
+            style={{ "--nav-item-icon": `url(${CalendarIcon})` }}
+            onClick={(e) => onNavbarItemClick(e)}>
             Reservations
           </p>
-          <p id="prepurchases" style={{ "--nav-item-icon": `url(${TakeawayIcon})` }} onClick={e => onNavbarItemClick(e)}>
+          <p
+            id="prepurchases"
+            style={{ "--nav-item-icon": `url(${TakeawayIcon})` }}
+            onClick={(e) => onNavbarItemClick(e)}>
             Pre-purchases
           </p>
-          <p id="reviews" style={{ "--nav-item-icon": `url(${CommentIcon})` }} onClick={e => onNavbarItemClick(e)}>
+          <p
+            id="reviews"
+            style={{ "--nav-item-icon": `url(${CommentIcon})` }}
+            onClick={(e) => onNavbarItemClick(e)}>
             Reviews
           </p>
 
@@ -123,10 +140,10 @@ export default function test() {
 
             <div>
               <h2 className={style["search"]}>How to search?</h2>
-              You can search for a speciffic attribute by prefixing your search data with the attribute and a colon:
-              <br/>
-              <br/>
-              
+              You can search for a speciffic attribute by prefixing your search
+              data with the attribute and a colon:
+              <br />
+              <br />
               <div>
                 for example:
                 <ul>
@@ -136,31 +153,36 @@ export default function test() {
                   <li>email:example@email.com</li>
                 </ul>
               </div>
-              <br/>
+              <br />
               <p className={style["error-disclaimer"]}>
-                If you encounter any unexpected data or results, immediately notify any of the developers and log out of your account.
+                If you encounter any unexpected data or results, immediately
+                notify any of the developers and log out of your account.
               </p>
             </div>
           </div>
 
-          <div className={style["searchbar"]}>
-            <input type="text" placeholder="Search..." />
-            <button>Search</button>
-
-            <button>Export</button>
-
-            <p>Entries per page:</p>
-              <input type="number" min="1" max="100" />
-          </div>
-
           <div className={style["table"]}>
+            <div className={style["searchbar"]}>
+              <div>
+                <input type="text" placeholder="Search..." />
+                <button>Search</button>
+
+                <button>Export</button>
+              </div>
+
+              <div>
+                <p>Entries per page:</p>
+                <input type="number" min="1" max="100" />
+              </div>
+            </div>
             <table>
               <thead>
                 <tr>
+                  <th>ID</th>
+                  <th>Username</th>
                   <th>First name</th>
                   <th>Last name</th>
                   <th>Email</th>
-                  <th>Username</th>
                   <th>Registration date</th>
                   <th>Last change</th>
                   <th>Actions</th>
@@ -168,37 +190,91 @@ export default function test() {
               </thead>
               <tbody>
                 <tr>
+                  <td>1</td>
+                  <td>john.doe</td>
                   <td>John</td>
                   <td>Doe</td>
                   <td>email@example.com</td>
-                  <td>john.doe</td>
                   <td>2021-12-13</td>
                   <td>2021-12-13</td>
                   <td>
-                    <button>View</button>
+                    <button>Edit</button>
+                    <button>Delete</button>
+                  </td>
+                </tr>
+                <tr>
+                  <td>1</td>
+                  <td>john.doe</td>
+                  <td>John</td>
+                  <td>Doe</td>
+                  <td>email@example.com</td>
+                  <td>2021-12-13</td>
+                  <td>2021-12-13</td>
+                  <td>
+                    <button>Edit</button>
+                    <button>Delete</button>
+                  </td>
+                </tr>
+                <tr>
+                  <td>1</td>
+                  <td>john.doe</td>
+                  <td>John</td>
+                  <td>Doe</td>
+                  <td>email@example.com</td>
+                  <td>2021-12-13</td>
+                  <td>2021-12-13</td>
+                  <td>
+                    <button>Edit</button>
+                    <button>Delete</button>
+                  </td>
+                </tr>
+                <tr>
+                  <td>1</td>
+                  <td>john.doe</td>
+                  <td>John</td>
+                  <td>Doe</td>
+                  <td>email@example.com</td>
+                  <td>2021-12-13</td>
+                  <td>2021-12-13</td>
+                  <td>
+                    <button>Edit</button>
+                    <button>Delete</button>
+                  </td>
+                </tr>
+                <tr>
+                  <td>1</td>
+                  <td>john.doe</td>
+                  <td>John</td>
+                  <td>Doe</td>
+                  <td>email@example.com</td>
+                  <td>2021-12-13</td>
+                  <td>2021-12-13</td>
+                  <td>
                     <button>Edit</button>
                     <button>Delete</button>
                   </td>
                 </tr>
               </tbody>
             </table>
-            
+
             <div className={style["pagination"]}>
-              <button>Previous</button>
-              <button>Next</button>
+              <div>
+                <button>Previous</button>
+                <p>Page 2 of 1</p>
+                <button>Next</button>
+              </div>
 
-              <p>Page 1 of 1</p>
+              <p>Showing <div>1</div> to <div>6</div> of <div>27435345345</div> entries</p>
 
-              <p>Showing 1 to 1 of 1 entries</p>
+              <div>
+                <p>Go to page:</p>
+                <input type="number" min="1" max="500" />
 
-              <p>Go to page:</p>
-              <input type="number" min="1" max="500" />
-
-              <button>Go</button>
+                <button>Go</button>
+              </div>
             </div>
           </div>
         </div>
-
       </div>
     </>
   )
