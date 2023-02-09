aa4a70540cf7ce8c42d16ca17d3c92bbd777075b
Alberts Liepins <liepins01@gmail.com>
2023-02-10 01:35:36 +0200

    feat: pushing directly to develop because i forgot to switch branch

--- dorsly/src/pages/profile.jsx
+++ dorsly/src/pages/profile.jsx
@@ -14,9 +14,175 @@ export default function profile() {
   const { user, token, setUser, setToken } = useContext(UserContext)
   const { popupData, createPopup, setPopupData } = useContext(PopupContext)
 
+  const exampleDealData = [
+    {
+      id: 70,
+      point_of_interest: {
+        id: 3,
+        name: "William Rosenbaum",
+        description:
+          "Beatae sed cupiditate et dolorem natus et impedit. Ipsum quia officiis ad sed asperiores nemo. Consectetur quia illo doloribus.",
+        images:
+          "https://via.placeholder.com/1920x1080.png/001100?text=food+placeat",
+      },
+      reservation: {
+        id: 70,
+        date: "2023-02-09 12:42:00",
+        number_of_people: 1,
+      },
+      pre_purchase: {
+        id: 70,
+        products: [
+          {
+            id: 40,
+            quantity: 4,
+          },
+          {
+            id: 63,
+            quantity: 3,
+          },
+          {
+            id: 282,
+            quantity: 1,
+          },
+        ],
+        total_price: "41.15",
+        status: "pending",
+        payment_status: "pending",
+        payment_id: null,
+      },
+      user_id: 1,
+      status: "expired",
+    },
+    {
+      id: 71,
+      point_of_interest: {
+        id: 7,
+        name: "Jessy Kozey",
+        description:
+          "Libero facere perferendis sequi consequatur. Itaque voluptas et consequatur et voluptatibus corrupti voluptate pariatur. Quas culpa ut eos nisi rerum aut. Delectus totam consequatur rerum ut neque.",
+        images:
+          "https://via.placeholder.com/1920x1080.png/00aa33?text=food+rerum",
+      },
+      reservation: {
+        id: 71,
+        date: "2023-02-09 12:42:00",
+        number_of_people: 1,
+      },
+      pre_purchase: {
+        id: 71,
+        products: [
+          {
+            id: 87,
+            quantity: 4,
+          },
+          {
+            id: 128,
+            quantity: 3,
+          },
+        ],
+        total_price: "89.62",
+        status: "pending",
+        payment_status: "pending",
+        payment_id: null,
+      },
+      user_id: 1,
+      status: "expired",
+    },
+    {
+      id: 72,
+      point_of_interest: {
+        id: 9,
+        name: "Keyon Brakus",
+        description:
+          "Quo ut doloribus eos et voluptatem ut veniam. Alias alias laudantium velit eum quae deleniti. Debitis debitis modi nesciunt cumque tempora.",
+        images:
+          "https://via.placeholder.com/1920x1080.png/00cc77?text=food+sunt",
+      },
+      reservation: {
+        id: 72,
+        date: "2023-02-09 12:42:00",
+        number_of_people: 1,
+      },
+      pre_purchase: {
+        id: 72,
+        products: [
+          {
+            id: 54,
+            quantity: 2,
+          },
+          {
+            id: 62,
+            quantity: 3,
+          },
+        ],
+        total_price: "19.17",
+        status: "pending",
+        payment_status: "open",
+        payment_id: null,
+      },
+      user_id: 1,
+      status: "expired",
+    },
+    {
+      id: 73,
+      point_of_interest: {
+        id: 10,
+        name: "Muhammad Turcotte",
+        description:
+          "Aliquid non minus voluptatibus aut qui. Dolorum qui expedita harum omnis veniam repellendus. Neque labore aliquid qui quidem. Ut dolor est ea eum dolor ex.",
+        images:
+          "https://via.placeholder.com/1920x1080.png/005500?text=food+est",
+      },
+      reservation: {
+        id: 73,
+        date: "2023-02-09 12:42:00",
+        number_of_people: 1,
+      },
+      pre_purchase: {
+        id: 73,
+        products: [
+          {
+            id: 152,
+            quantity: 2,
+          },
+          {
+            id: 205,
+            quantity: 3,
+          },
+        ],
+        total_price: "50.33",
+        status: "complete",
+        payment_status: "open",
+        payment_id:
+          "cs_test_b1gVgoOUecfQLEF1LDBUcR987tenkG5tBeLeYaT4Rn6uBt4eZL8dXQln2V",
+      },
+      user_id: 1,
+      status: "completed",
+    },
+    {
+      id: 74,
+      point_of_interest: {
+        id: 14,
+        name: "Alaina Hauck MD",
+        description:
+          "Ut ea porro perspiciatis labore ipsam est et. Assumenda voluptatibus ea totam eligendi eius repellendus ipsam. Tempore minus quis corporis impedit voluptates. Minus maxime iusto qui ut a.",
+        images:
+          "https://via.placeholder.com/1920x1080.png/0022dd?text=food+consequatur",
+      },
+      reservation: {
+        id: 74,
+        date: "2023-02-10 12:42:00",
+        number_of_people: 5,
+      },
+      user_id: 1,
+      status: "active",
+    },
+  ]
+
   const [section, setSection] = useState("reservations")
 
-  const navigate = useNavigate() 
+  const navigate = useNavigate()
 
   useEffect(() => {
     document.body.style.backgroundImage = ""
@@ -42,13 +208,96 @@ export default function profile() {
   const renderSection = (section) => {
     switch (section) {
       case "reservations":
-        return <p>Reservations</p>
+        const renderDeals = () => {
+          // {
+          //   id: 70,
+          //   point_of_interest: {
+          //     id: 3,
+          //     name: "William Rosenbaum",
+          //     description:
+          //       "Beatae sed cupiditate et dolorem natus et impedit. Ipsum quia officiis ad sed asperiores nemo. Consectetur quia illo doloribus.",
+          //     images:
+          //       "https://via.placeholder.com/1920x1080.png/001100?text=food+placeat",
+          //   },
+          //   reservation: {
+          //     id: 70,
+          //     date: "2023-02-09 12:42:00",
+          //     number_of_people: 1,
+          //   },
+          //   pre_purchase: {
+          //     id: 70,
+          //     products: [
+          //       {
+          //         id: 40,
+          //         quantity: 4,
+          //       },
+          //       {
+          //         id: 63,
+          //         quantity: 3,
+          //       },
+          //       {
+          //         id: 282,
+          //         quantity: 1,
+          //       },
+          //     ],
+          //     total_price: "41.15",
+          //     status: "pending",
+          //     payment_status: "pending",
+          //     payment_id: null,
+          //   },
+          //   user_id: 1,
+          //   status: "expired",
+          // }
+
+          return exampleDealData.map((deal, index) => {
+            return (
+              <div className={style["deal"]} key={index}>
+                <div className={style["reservation-data"]}>
+                  <img src={deal.point_of_interest.images} />
+                  <div className={style["reservation-data-content"]}>
+                    <h1>{deal.point_of_interest.name}</h1>
+                    <p>{deal.point_of_interest.description}</p>
+                  </div>
+                  <div className={style["reservation-secondary-data-content"]}>
+                    <div>
+                      <p>Reservation date</p>
+                      <p>{deal.reservation.date}</p>
+                    </div>
+                    <div>
+                      <p>Number of people</p>
+                      <p>{deal.reservation.number_of_people}</p>
+                    </div>
+                    <div>
+                      <p>Status</p>
+                      <p>{deal.status}</p>
+                    </div>
+                  </div>
+                </div>
+                <div
+                  aria-hidden={true}
+                  className={style["pre-purchase-data"]}></div>
+              </div>
+            )
+          })
+        }
+
+        return (
+          <>
+            <h1>Reservations</h1>
+
+            <div className={style["deals"]}>{renderDeals()}</div>
+          </>
+        )
+
       case "reviews":
         return <p>Reviews</p>
+
       case "ratings":
         return <p>Ratings</p>
+
       case "settings":
         return <p>Settings</p>
+
       default:
         return <p>Reservations</p>
     }
