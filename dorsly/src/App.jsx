59876de57b54ad620171b2882a360388e77c88ce
Alberts Liepins <liepins01@gmail.com>
2023-02-22 23:56:21 +0200

    feat: redirects and getting form data

--- dorsly/src/App.jsx
+++ dorsly/src/App.jsx
@@ -3,16 +3,16 @@ import { BrowserRouter, Route, Routes } from "react-router-dom"
 
 import "../src/static/css/general.css"
 
-import MainProductPage from "./pages/mainProductPage"
-import Authentificaton from "./pages/authentificaton"
-import PaymentGateway from "./pages/paymentGateway"
-import AboutUs from "./pages/aboutUs";
-import Instruction from "./pages/instruction"
-import Profile from "./pages/profile"
-import Place from "./pages/place"
-import Error from "./pages/error"
+import AboutUs from "./pages/aboutUs"
 import Admin from "./pages/admin"
+import Authentificaton from "./pages/authentificaton"
+import Error from "./pages/error"
 import Home from "./pages/home"
+import Instruction from "./pages/instruction"
+import MainProductPage from "./pages/mainProductPage"
+import PaymentGateway from "./pages/paymentGateway"
+import Place from "./pages/place"
+import Profile from "./pages/profile"
 
 export default function App() {
   return (
@@ -25,11 +25,11 @@ export default function App() {
         <Route path="/profile" element={<Profile />} />
         <Route path="/login" element={<Authentificaton page="login" />} />
         <Route path="/place" element={<Place />} />
-        <Route path="/profile" element={<Profile/>} />
-        <Route path="/error" element={<Error/>} />
-        <Route path="/admin" element={<Admin/>} />
-        <Route path="/aboutUs" element={<AboutUs/>} />
-        <Route path="/" element={<Home/>} />
+        <Route path="/profile" element={<Profile />} />
+        <Route path="/error" element={<Error />} />
+        <Route path="/admin" element={<Admin />} />
+        <Route path="/aboutUs" element={<AboutUs />} />
+        <Route path="/" element={<Home />} />
       </Routes>
     </BrowserRouter>
   )
