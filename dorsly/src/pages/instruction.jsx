298dc2e1fea53d10b593873a6f848e6aa16bffce
roberttss <robertsabolins772@gmail.com>
2023-02-22 21:34:30 +0200

    instruction page progres

--- dorsly/src/pages/instruction.jsx
+++ dorsly/src/pages/instruction.jsx
@@ -9,8 +9,19 @@ export default function instruction() {
   return (
     <>
       <Header />
-
+      
       <div className={style["container"]}>
+        <div className={style["button__container"]}>
+      <button className={style["instruction__button"]}>Administrācijas lapas navigācijas aile </button>
+      <button className={style["instruction__button"]}>Admin lapas datu izvade </button>
+      <button className={style["instruction__button"]}>Profila lapas esošais progress </button>
+      <button className={style["instruction__button"]}>Reitngs </button>
+      <button className={style["instruction__button"]}>Rezervacija </button>
+      <button className={style["instruction__button"]}>Restorāna informācija </button>
+      <button className={style["instruction__button"]}>Rezervācijas kommenti  </button>
+      <button className={style["instruction__button"]}>Produktu lapa </button>
+        </div>
+      <div>
         <div>
         <h1>
         Administrācijas lapas navigācijas aile
@@ -39,35 +50,35 @@ export default function instruction() {
     <br/>
 
     <h1>
-      RATING
+      Reitngs
         </h1>
-        <p>  Ieejot restorānā mājas lapiņa , jūs ieraudzisiet 6-šu zvaigžņu reitinga skalu  , ar kuru palīdzību jūs varat ērti novērtēt restorānu , gan vienkārši uzvilkot ar peli uz jūsu vērtējumu , gan uzklikšķinot uz atbilstoša zvaigznīša </p>
+        <p>  Ieejot restorānā mājas lapiņa , jūs ieraudzisiet 6-šu zvaigžņu reitinga skalu  , ar kuru palīdzību jūs varat ērti novērtēt restorānu , gan vienkārši uzvilkot ar peli uz jūsu vērtējumu , gan uzklikšķinot uz atbilstoša zvaigznīša.</p>
         <br/>
 
         <h1>
-        RESERV
+        Rezervacija
         </h1>
-        <p> Izvēlējāmies nepieciešamo datumu , vai uzklikšķinot uz kalendāra, vai paši ierakstot nepieciešamos datus , laiku un cilvēku skaitu . Uzspiežam reserve podziņu , un mēs esam veiksmīgi rezervējuši galdiņu </p>
+        <p> Izvēlējāmies nepieciešamo datumu , vai uzklikšķinot uz kalendāra, vai paši ierakstot nepieciešamos datus , laiku un cilvēku skaitu . Uzspiežam reserve podziņu , un mēs esam veiksmīgi rezervējuši galdiņu.</p>
         <br/> 
 
         <h1>
-        REST. INFO
+        Restorāna informācija
         </h1>
-        <p> Izvēlējāmies info sadaļu , un tur mēs ieraudzam : restorāna aprakstu un restorāna lokaciju , kuru mēs karti varam apskatīt precīzāk </p>
+        <p> Izvēlējāmies info sadaļu , un tur mēs ieraudzam : restorāna aprakstu un restorāna lokaciju , kuru mēs karti varam apskatīt precīzāk.</p>
         <br/>
 
         <h1>
-        REV. COMMENTS 
+        Rezervācijas kommenti 
         </h1>
-        <p> Ieejot komentāra/reitinga sadaļu , varam atstāt savu komentāru , kurām jābūt vismaz 10 simboliem un ne vairāk par 255 simboliem , arī varam apskatīt jau esošu komentārus vai izdzēst savējo</p>
+        <p> Ieejot komentāra/reitinga sadaļu , varam atstāt savu komentāru , kurām jābūt vismaz 10 simboliem un ne vairāk par 255 simboliem , arī varam apskatīt jau esošu komentārus vai izdzēst savējo.</p>
         <br/>
 
         <h1>
-        PRODUCT PURCHASE  
+        Produktu lapa 
         </h1>
-        <p> Produktu sadaļā , varam apskatīt un  izvēlēties pārtiku , uzspiežot uz tā mes viņu uzreiz pievienojam grozā . Grozā iekšā varam ieraudzīt kopējo summu , kā arī izvēlēties pārtikas daudzumu , izņemot produktu no groza , viņš automātiski arī noņemas no summas kā arī noņemas no galvenās lapas Uzspiežot "Pay now" pogu , mūs uzreiz aizsūtīt uz online apmaksu. Teksts ko likt tajā aprakstā ^</p>
+        <p> Produktu sadaļā , varam apskatīt un  izvēlēties pārtiku , uzspiežot uz tā mes viņu uzreiz pievienojam grozā . Grozā iekšā varam ieraudzīt kopējo summu , kā arī izvēlēties pārtikas daudzumu , izņemot produktu no groza , viņš automātiski arī noņemas no summas kā arī noņemas no galvenās lapas Uzspiežot "Pay now" pogu , mūs uzreiz aizsūtīt uz online apmaksu. Teksts ko likt tajā aprakstā.</p>
         <br/>
-
+        </div>
       </div>
     </>
   )
